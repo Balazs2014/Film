c266d8588e5f8087b1a5c463a5d301acbf78a4eb
Balazs2014 <tbalazs.levente@gmail.com>
2021-10-05 15:47:08 +0200

    Bootstrap, css meghívása, végső szépítések

--- main.js
+++ main.js
@@ -14,24 +14,30 @@ function listaFeltolt() {
     let rendezo = document.getElementById('rendezo').value;
     let kiadas_eve = document.getElementById('kiadas_eve').value;
 
-    lista.push(new Film(nev, rendezo, kiadas_eve));
+    if (nev != "" && rendezo != "" && kiadas_eve != "") {
+        let f = new Film(nev, rendezo, kiadas_eve);
+        lista.push(f);
+    }
+    
+    filmekKilistazas();
 
     document.getElementById('nev').value = "";
     document.getElementById('rendezo').value = "";
-    document.getElementById('kiadas_eve').value = "";
+    document.getElementById('kiadas_eve').value = "";  
 }
 
 function filmekKilistazas() {
-    let s = "<table><tr><th>Név</th><th>Rendező</th><th>Kiadási év</th></tr>";
-    for (let i = 0; i < lista.length; i++) {
-        s += "<tr><td>" + lista[i].nev + "</td><td>" + lista[i].rendezo + "</td><td>" + lista[i].kiadas_eve + "</td></tr>";
+    if (lista.length !== 0) {
+        let s = "<table class='table text-center table-striped'><tr><th>Név</th><th>Rendező</th><th>Kiadási év</th></tr>";
+        for (let i = 0; i < lista.length; i++) {
+            s += "<tr><td>" + lista[i].nev + "</td><td>" + lista[i].rendezo + "</td><td>" + lista[i].kiadas_eve + "</td></tr>";
+        }
+
+        s += "</table>";
+
+        document.getElementsByTagName('table').innerHTML = "";
+        document.getElementById('tablazat').innerHTML = s;
     }
-
-    s += "</table>";
-
-    document.getElementsByTagName('table').innerHTML = "";
-    document.getElementById('tablazat').innerHTML = s;
 }
 
 document.getElementById('hozzaad').addEventListener('click', listaFeltolt);
-document.getElementById('listazas').addEventListener('click', filmekKilistazas);
