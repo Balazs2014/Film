c03e2dfe00a98f95eb54d0106a7dba7dceb0dce9
Balazs2014 <tbalazs.levente@gmail.com>
2021-10-05 14:39:26 +0200

    Lista felttöltése, Hozzáad gomb

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let filmTomb = [];
+let lista = [];
 
 class Film {
 
@@ -8,3 +8,17 @@ class Film {
         this.kiadas_eve = kiadas_eve;
     }
 }
+
+function listaFeltolt() {
+    let nev = document.getElementById('nev').value;
+    let rendezo = document.getElementById('rendezo').value;
+    let kiadas_eve = document.getElementById('kiadas_eve').value;
+
+    filmTomb.push(new Film(nev, rendezo, kiadas_eve));
+
+    document.getElementById('nev').value = "";
+    document.getElementById('rendezo').value = "";
+    document.getElementById('kiadas_eve').value = "";
+}
+
+document.getElementById('hozzaad').addEventListener('click', listaFeltolt);
