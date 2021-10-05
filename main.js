bb1960c55e4cdce7ef9eb61fad29f28b608f601e
Balazs2014 <tbalazs.levente@gmail.com>
2021-10-05 14:11:59 +0200

    Film osztály létrehozása, adattagok deklarálása Beviteli mezők létrehozása

---
+++ main.js
@@ -0,0 +1,10 @@
+let filmTomb = [];
+
+class Film {
+
+    constructor(nev, rendezo, kiadas_eve) {
+        this.nev = nev;
+        this.rendezo = rendezo;
+        this.kiadas_eve = kiadas_eve;
+    }
+}
