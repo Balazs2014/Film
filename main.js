b07c654152eedba1426f05029c4fefe2a39c4e34
Balazs2014 <tbalazs.levente@gmail.com>
2021-10-05 14:57:35 +0200

    Kiíratás táblázatba

--- main.js
+++ main.js
@@ -14,11 +14,24 @@ function listaFeltolt() {
     let rendezo = document.getElementById('rendezo').value;
     let kiadas_eve = document.getElementById('kiadas_eve').value;
 
-    filmTomb.push(new Film(nev, rendezo, kiadas_eve));
+    lista.push(new Film(nev, rendezo, kiadas_eve));
 
     document.getElementById('nev').value = "";
     document.getElementById('rendezo').value = "";
     document.getElementById('kiadas_eve').value = "";
 }
 
+function filmekKilistazas() {
+    let s = "<table><tr><th>Név</th><th>Rendező</th><th>Kiadási év</th></tr>";
+    for (let i = 0; i < lista.length; i++) {
+        s += "<tr><td>" + lista[i].nev + "</td><td>" + lista[i].rendezo + "</td><td>" + lista[i].kiadas_eve + "</td></tr>";
+    }
+
+    s += "</table>";
+
+    document.getElementsByTagName('table').innerHTML = "";
+    document.getElementById('tablazat').innerHTML = s;
+}
+
 document.getElementById('hozzaad').addEventListener('click', listaFeltolt);
+document.getElementById('listazas').addEventListener('click', filmekKilistazas);
